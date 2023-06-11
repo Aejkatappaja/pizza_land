6e444338e417a85997ec1035548c953ce7be1255
Frank Jover <joverjlj@gmail.com>
2023-06-11 22:50:46 +0300

    banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Banner } from "./components/Banner";
 import { Pizza } from "./components/Pizza";
 import { getAllPizzas } from "./lib/pizzas";
 import { PizzaType } from "./types/types";
@@ -6,11 +7,10 @@ export default async function Home() {
   const pizzas = await getAllPizzas();
   return (
     <section>
+      <Banner />
       <div className="container mx-auto">
         <div className="grid grid-cols-2 gap-[15px] md:grid-cols-3 xl:grid-cols-4 xl:gap-[30px] py-12">
           {pizzas.map((pizza: PizzaType) => {
-            console.log(pizza);
-
             return <Pizza key={pizza.id.toString()} pizza={pizza} />;
           })}
         </div>
