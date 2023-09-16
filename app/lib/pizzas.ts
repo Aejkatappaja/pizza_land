7bdd19b5279c9844dae3a3f8ff0a66cb363e4593
Aejkatappaja <joverjlj@gmail.com>
2023-09-16 20:37:10 +0200

    refacto: using env.local instead of hard coding url

--- app/lib/pizzas.ts
+++ app/lib/pizzas.ts
@@ -1,7 +1,9 @@
 import { PizzaType } from "@/types/types";
 
 export async function getAllPizzas(): Promise<PizzaType[]> {
-  const res = await fetch(`https://api.jsonserve.com/8PFXVY`);
+  const URL = process.env.DATA_URL;
+
+  const res = await fetch(`${URL}`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
