8e1bdda0bf7ea9ea6888b5f496a6e191f4a1073e
Aejkatappaja <joverjlj@gmail.com>
2023-09-18 22:22:29 +0200

    chore: -D Modal & mkdir containing all features

---
+++ app/components/modal/index.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import React from 'react';
+
+import { useVisibleContext } from '@/context/isVisibleContext';
+import { SelectPizzaOptions } from './pizza-options-modal';
+import { CheckoutModal } from './checkout-modal';
+
+export const Modal = () => {
+  const { isVisible, closeModal, isCheckoutVisible, setIsCheckoutVisible } =
+    useVisibleContext();
+
+  return isVisible ? (
+    <section
+      className='fixed left-0 top-0 z-30 flex h-[100vh] w-[100vw] items-center justify-center bg-gray-600 bg-opacity-25'
+      onClick={() => {
+        {
+          !isCheckoutVisible && closeModal();
+        }
+      }}
+    >
+      {isCheckoutVisible ? <CheckoutModal /> : <SelectPizzaOptions />}
+    </section>
+  ) : null;
+};
