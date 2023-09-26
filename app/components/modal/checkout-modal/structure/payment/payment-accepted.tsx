12822621d3431291b5947541b12a8f0da2282e1b
Aejkatappaja <joverjlj@gmail.com>
2023-09-26 22:24:13 +0200

    add: props drilling

--- app/components/modal/checkout-modal/structure/payment/payment-accepted.tsx
+++ app/components/modal/checkout-modal/structure/payment/payment-accepted.tsx
@@ -3,13 +3,17 @@ import Image from 'next/image';
 
 interface PaymentAcceptedProps {
   count: number;
+  customerIdentity: string | null;
 }
 
-export const PaymentAccepted: React.FC<PaymentAcceptedProps> = ({ count }) => {
+export const PaymentAccepted: React.FC<PaymentAcceptedProps> = ({
+  count,
+  customerIdentity,
+}) => {
   return (
     <div className='z-50 flex h-[100vh] flex-col items-center justify-center px-6 lg:h-[600px]'>
       <h2 className='text-center text-2xl font-semibold'>
-        Thank you ! The order has been placed !
+        Thank you {customerIdentity} ! The order has been placed !
       </h2>
       <Image
         src={'/success-1.gif'}
